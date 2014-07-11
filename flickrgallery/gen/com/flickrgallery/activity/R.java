6396cdbe39f5250c83adcea45360a9cc856fa4f9
Umang Desai <umang12desai@gmail.com>
2014-07-11 14:20:15 -0500

    search feature added

--- flickrgallery/gen/com/flickrgallery/activity/R.java
+++ flickrgallery/gen/com/flickrgallery/activity/R.java
@@ -28,6 +28,7 @@ public final class R {
         public static final int gridView1=0x7f080000;
         public static final int imageTitle=0x7f080003;
         public static final int imageView1=0x7f080002;
+        public static final int menu_search=0x7f080005;
         public static final int progressBar1=0x7f080001;
     }
     public static final class layout {
@@ -41,6 +42,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int search_txt=0x7f050003;
     }
     public static final class style {
         /** 
